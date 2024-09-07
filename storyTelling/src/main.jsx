b513cc3923ace4fd795498c68c7749484c74bb29
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-09-07 15:20:45 +0600

    axios and router-dom installed

--- storyTelling/src/main.jsx
+++ storyTelling/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+import { BroserRouter as Router} from 'react-router-dom'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
 )
